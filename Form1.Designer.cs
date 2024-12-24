6ee49fe7dbae7202118eb2f3ed2edf163778ca81
ChanwitAlex <chanwit.u@kkumail.com>
2024-12-24 11:38:33 +0700

    add

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            chbCoffee = new CheckBox();
+            chbGreentea = new CheckBox();
+            label1 = new Label();
+            button1 = new Button();
+            tbCoffeePrice = new TextBox();
+            tbGreenTeaPrice = new TextBox();
+            tbTotal = new TextBox();
+            tbCoffeeQuantity = new TextBox();
+            tbGreenTeaQuantity = new TextBox();
+            SuspendLayout();
+            // 
+            // chbCoffee
+            // 
+            chbCoffee.AutoSize = true;
+            chbCoffee.Location = new Point(120, 23);
+            chbCoffee.Name = "chbCoffee";
+            chbCoffee.Size = new Size(61, 19);
+            chbCoffee.TabIndex = 0;
+            chbCoffee.Text = "Coffee";
+            chbCoffee.UseVisualStyleBackColor = true;
+            // 
+            // chbGreentea
+            // 
+            chbGreentea.AutoSize = true;
+            chbGreentea.Location = new Point(120, 48);
+            chbGreentea.Name = "chbGreentea";
+            chbGreentea.Size = new Size(78, 19);
+            chbGreentea.TabIndex = 1;
+            chbGreentea.Text = "Green Tea";
+            chbGreentea.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(313, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Total";
+            label1.Click += but;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(504, 28);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 124);
+            button1.TabIndex = 3;
+            button1.Text = "Check out";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tbCoffeePrice
+            // 
+            tbCoffeePrice.Location = new Point(246, 21);
+            tbCoffeePrice.Name = "tbCoffeePrice";
+            tbCoffeePrice.Size = new Size(100, 23);
+            tbCoffeePrice.TabIndex = 4;
+            // 
+            // tbGreenTeaPrice
+            // 
+            tbGreenTeaPrice.Location = new Point(246, 50);
+            tbGreenTeaPrice.Name = "tbGreenTeaPrice";
+            tbGreenTeaPrice.Size = new Size(100, 23);
+            tbGreenTeaPrice.TabIndex = 5;
+            // 
+            // tbTotal
+            // 
+            tbTotal.Location = new Point(375, 124);
+            tbTotal.Name = "tbTotal";
+            tbTotal.Size = new Size(100, 23);
+            tbTotal.TabIndex = 6;
+            // 
+            // tbCoffeeQuantity
+            // 
+            tbCoffeeQuantity.Location = new Point(375, 19);
+            tbCoffeeQuantity.Name = "tbCoffeeQuantity";
+            tbCoffeeQuantity.Size = new Size(100, 23);
+            tbCoffeeQuantity.TabIndex = 7;
+            // 
+            // tbGreenTeaQuantity
+            // 
+            tbGreenTeaQuantity.Location = new Point(375, 50);
+            tbGreenTeaQuantity.Name = "tbGreenTeaQuantity";
+            tbGreenTeaQuantity.Size = new Size(100, 23);
+            tbGreenTeaQuantity.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(637, 222);
+            Controls.Add(tbGreenTeaQuantity);
+            Controls.Add(tbCoffeeQuantity);
+            Controls.Add(tbTotal);
+            Controls.Add(tbGreenTeaPrice);
+            Controls.Add(tbCoffeePrice);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(chbGreentea);
+            Controls.Add(chbCoffee);
+            Name = "Form1";
+            Text = "Chan";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CheckBox chbCoffee;
+        private CheckBox chbGreentea;
+        private Label label1;
+        private Button button1;
+        private TextBox tbCoffeePrice;
+        private TextBox tbGreenTeaPrice;
+        private TextBox tbTotal;
+        private TextBox tbCoffeeQuantity;
+        private TextBox tbGreenTeaQuantity;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,57 @@ namespace ShoppingCart
         {
             InitializeComponent();
         }
+
+        private void but(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                //get amount 
+                string strCoffeePrice = tbCoffeePrice.Text;
+                string strGreenTeaPrice = tbGreenTeaPrice.Text;
+                
+                
+                //get price
+                string strCoffeeQuantity = tbCoffeeQuantity.Text;
+                string strGreenTeaQuantity = tbGreenTeaQuantity.Text;
+
+                double iCoffeePrice = 0;
+                double iGreenTeaPrice= 0;
+                double iCoffeeQuantity = 0;
+                double iGreenTeaQuantity = 0;
+            try
+            {
+                //covert string to int
+                if (chbCoffee.Checked)
+                {
+                    iCoffeePrice = double.Parse(strCoffeePrice);
+                    iCoffeeQuantity = double.Parse(strCoffeeQuantity);
+                }
+                if (chbGreentea.Checked)
+                {
+                    iGreenTeaPrice = double.Parse(strGreenTeaPrice);
+                    iGreenTeaQuantity = double.Parse(strGreenTeaQuantity);
+                }
+            }
+            catch (Exception ex)
+            {
+                 iCoffeePrice = 0;
+                 iCoffeeQuantity = 0;
+                 iGreenTeaPrice = 0;
+                 iGreenTeaQuantity = 0;
+
+            }
+        //calculate total
+        double iCoffee = iCoffeePrice * iCoffeeQuantity;
+        double iGreenTea =iGreenTeaPrice * iGreenTeaQuantity;
+        double iTotal = iCoffee + iGreenTea;
+        //display total
+        tbTotal.Text = iTotal.ToString();
+
+        }
     }
-}
+    }
+
